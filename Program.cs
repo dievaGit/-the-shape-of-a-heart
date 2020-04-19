00aa3ff2a66de7e93ca30291d4b8706e662ed036
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:41:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //building the heart
+            Console.WriteLine(" O O   O O \nO   O O   O\n O   O   O \n  O     O  \n   O   O   \n    O O    \n     O     ");
+
+            Console.ReadKey();
+           
+        }
+    }
+}
